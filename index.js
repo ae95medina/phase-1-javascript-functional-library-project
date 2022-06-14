7492d31cb44d688ae093690231202276dce5e62b
ae95medina <ae95medina@gmail.com>
2022-06-14 18:49:22 -0500

    commit tree 41464cc9537057b81cc431ba8f0febc47629bd73

---
+++ index.js
@@ -0,0 +1,13 @@
+function myEach(testArr, alert){
+  const values = Object.values(testArr)
+  values.forEach(value => alert(value))
+
+return testArr
+}
+function myMap(testArr, callback){
+const newArray = []
+const values = Object.values(testArr).map(testArr => {
+    return newArray
+})
+console.log(values, newArray)
+}
